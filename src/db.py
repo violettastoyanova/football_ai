dfae8aba525b1f61af6e44ff1c89fd0124ead19a
Violeta <vili@Violetas-MacBook-Air.local>
2026-02-26 01:56:53 +0200

    Updated db logic and intents, improved README

--- src/db.py
+++ src/db.py
@@ -1,15 +1,9 @@
 import sqlite3
 import os
 
-
-
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))
 DB_PATH = os.path.join(PROJECT_ROOT, "football.db")
-
-
-
 
 def get_connection():
    try:
@@ -19,31 +13,21 @@ def get_connection():
    except sqlite3.Error as e:
        print("Грешка при връзка с базата:", e)
 
-
-
-
 def initialize_database():
    schema_path = os.path.join(PROJECT_ROOT, "sql", "schema.sql")
-
 
    if not os.path.exists(schema_path):
        print("schema.sql не е намерен.")
        return
 
-
    conn = get_connection()
-
 
    with open(schema_path, "r", encoding="utf-8") as file:
        schema_sql = file.read()
 
-
    conn.executescript(schema_sql)
    conn.commit()
    conn.close()
-
-
-
 
 def execute_query(query, params=(), fetch=False):
    try:
@@ -51,17 +35,14 @@ def execute_query(query, params=(), fetch=False):
        cursor = conn.cursor()
        cursor.execute(query, params)
 
-
        if fetch:
            result = cursor.fetchall()
            conn.close()
            return result
 
-
        conn.commit()
        conn.close()
        return True
-
 
    except sqlite3.Error as e:
        return f"Database error: {e}"
